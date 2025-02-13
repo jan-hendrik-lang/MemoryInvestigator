fa84471a9d6f432dd530c9f7774079eb59b865da
jan-hendrik-lang <jan-hendriklang@hotmail.de>
2025-02-13 13:28:28 +0100

    deprecated langchain.chat_models change to langchain_community.chat_models

--- utils/initialize_rag_chat.py
+++ utils/initialize_rag_chat.py
@@ -1,5 +1,5 @@
 from langchain_google_genai import GoogleGenerativeAI
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
